6b6899073f2c8905ee455cc3cf4496a21841595e
Lukas Normantas <spam@discoverops.com>
2018-11-04 20:14:20 +0000

    Certificates and CA generated before with `make install`
    Currently still needs to be imported into Chrome clicking menus

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 install:
 	docker run \
+		--user $(id -u):$(id -g) \
 		-v $(PWD)/generate-certs.sh:/generate-certs.sh \
 		-v $(PWD)/mitm/certs:/certs \
 		-v $(PWD)/chrome/ca:/ca \
--- generate-certs.sh
+++ generate-certs.sh
@@ -23,13 +23,25 @@ openssl genrsa -out /certs/google.com.key 2048
 
 # Create a CSR
 openssl req -new -key /certs/google.com.key \
-    -subj "/C=DE/ST=NRW/L=Berlin/O=o/OU=pps/CN=www/emailAddress=email" \
+    -subj "/C=DE/ST=NRW/L=Berlin/O=o/OU=pps/CN=google.com/emailAddress=email" \
     -out /certs/google.com.csr
+
+cat >/certs/google.com.ext <<EOL
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = google.com
+DNS.2 = www.google.com
+EOL
 
 openssl x509 -req -in /certs/google.com.csr \
     -CA /ca/root.pem \
     -CAkey /ca/ca.key -CAcreateserial \
-    -out /certs/google.com.crt -days 1825 -sha256
+    -out /certs/google.com.crt -days 1825 -sha256 \
+    -extfile /certs/google.com.ext
 
 ls /ca -las
 ls /certs -las
